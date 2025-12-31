66083314f55cbfc6e8959a3a45174c81286b6363
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2025-12-31 09:27:46 +0000

    add

--- api/utils.ts
+++
@@ -1,6 +0,0 @@
-export const addCorsHeaders = (allowOrigin: string, response: Response) => {
-  response.headers.set('Access-Control-Allow-Origin', allowOrigin);
-  response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-  return response;
-};
